7701ff239e504404408874f404b9be5f10fe005e
Andrii Holovan <vozhd1908@gmail.com>
2024-07-28 12:56:40 +0200

    feat: extend eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,31 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
+    'airbnb-typescript',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json'],
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {'devDependencies': true},
+    ]
   },
 }
