4ef1815cf0791d7bbf0e189bc6638e7f7494ea9b
Andrii Holovan <vozhd1908@gmail.com>
2026-03-05 00:08:50 +0100

    chore: update tanstack router plugin usage

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
 });
